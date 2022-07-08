34879d1d0a47257acf7f5c27e5c266b21589776e
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-07-07 21:11:27 -0300

    ✨ Add basic stat tracking for `Machine` and `Source`

--- manufacturing_line/models/machine.py
+++ manufacturing_line/models/machine.py
@@ -16,15 +16,39 @@ class Machine:
 class _Machine:
 
     def __init__(self, env:simpy.Environment, machine:Machine, objects:dict):
-        self.env = env
+        
+        # Properties
         self.name = machine.name
         self.input_buffer = objects[machine.input_buffer]
         self.output_buffer = objects[machine.output_buffer]
         self.speed = machine.speed
+
+        # Stats
+        self.time_starved = 0
+        self.time_processing = 0
+        self.time_blocked = 0
+        self.items_processed = 0
+        
+        # Environment
+        self.env = env
         self.process = self.env.process(self.produce())
+
 
     def produce(self):
         while True:
+
+            # Starving
+            starving_start = self.env.now
             part = yield self.input_buffer.buffer.get()
+            processing_start = self.env.now
+            self.time_starved += (processing_start-starving_start)
+
+            # Processing
             yield self.env.timeout(self.speed)
+            blocked_start = self.env.now
+            self.items_processed += 1
+            self.time_processing += (blocked_start-processing_start)
+
+            # Blocked
             yield self.output_buffer.buffer.put(part)
+            self.time_blocked += (self.env.now-blocked_start)
--- manufacturing_line/models/source.py
+++ manufacturing_line/models/source.py
@@ -15,13 +15,28 @@ class Source:
 class _Source:
 
     def __init__(self, env:simpy.Environment, source:Source, objects:dict):
-        self.env = env
+        
+        # Properties
         self.name = source.name
         self.output_buffer = objects[source.output_buffer]
         self.speed = source.speed
+
+        # Stats
+        self.items_created = 0
+        self.time_blocked = 0
+
+        # Environment
+        self.env = env
         self.process = self.env.process(self.produce())
 
     def produce(self):
         while True:
+
+            # Create
             yield self.env.timeout(self.speed)
+            self.items_created += 1
+
+            # Blocked
+            blocked_start = self.env.now
             yield self.output_buffer.buffer.put(1)
+            self.time_blocked += (self.env.now-blocked_start)
