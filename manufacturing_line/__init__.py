09acd7d5780a30c7b55eff8ce09d04dc9df5e8cb
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-07-11 20:57:44 -0300

    🎨 Expand `__init__.py`

--- manufacturing_line/__init__.py
+++ manufacturing_line/__init__.py
@@ -1,4 +1,12 @@
+"""Initialization module."""
+
 from .models.buffer import Buffer
 from .models.machine import Machine
 from .models.line import Line
 from .models.source import Source
+
+
+
+__author__ = 'Gustavo Furtado'
+__email__ = 'gustavofurtado2@gmail.com'
+__version__ = '0.0.1'
