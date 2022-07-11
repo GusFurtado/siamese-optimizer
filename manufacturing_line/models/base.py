e4082188cb56375c0cfbc0a04ee643d9757c27de
Gustavo Furtado <gustavofurtado2@gmail.com>
2022-07-11 20:55:56 -0300

    🎨 Expand `base` submodule

---
+++ manufacturing_line/models/base.py
@@ -0,0 +1,13 @@
+"""Submodule for base models."""
+
+
+
+class Model:
+    """Base model type."""
+    pass
+
+
+
+class Equipment(Model):
+    """An physical asset the transforms a production unit."""
+    pass
